c4ab2f0258632f7e97a259bbc9648e55e30de0e8
Saurabh Kabadi <68393690+Saurabh-DS@users.noreply.github.com>
2020-12-12 21:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,70 @@ from resizeimage import resizeimage
 from flask import Flask, request, render_template, send_from_directory
 from skimage import transform
 
+app = Flask(__name__)
+model = tf.keras.models.load_model("MNIST_classifier_nn_model.h5")
+
+@app.route('/')
+def home():
+    return render_template('FashionClassifier.html')
+
+@app.route('/predict',methods=['POST'])
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('FashionClassifier.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+'''import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from resizeimage import resizeimage
+from flask import Flask, request, render_template, send_from_directory
+from skimage import transform
+
 # Your directory name where images should be uploaded
 UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
@@ -65,7 +129,7 @@ def create_app():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
-    
+    '''
     
 
 '''
